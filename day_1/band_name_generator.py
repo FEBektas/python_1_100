0df7bcb6e8f8bad30cf059d7cbf28ebd644d1abb
Bektas Erkan Florin <b.e.florin@outlook.com>
2025-04-28 14:18:43 +0300

    Add files via upload

---
+++ day_1/band_name_generator.py
@@ -0,0 +1,8 @@
+import random
+
+print("Wellcome to the Band Name Generator!")
+
+city = input("What is the name of the city you grew up in?\n")
+fav_word = input("What is your favorite word?\n")
+
+print(f"Your band name can be {city} {fav_word}")
---
+++ day_1/task.py
@@ -0,0 +1,22 @@
+# input for the user's name
+# name = input("What is your name? ")
+
+# print statements
+print("Hello World!, 'This is the first line of code'")
+print("Hello World!,\nHello world!,\nanother 'Hello World!'")
+print(f"Hello " + "Erkan")
+# print(f"Hello {name}!")
+# print("Hello " + input("What is your name? ") + "!")
+
+# Variables
+age = 25
+print(age)
+
+age = 30
+print(age)
+
+# Finding the length of the name by assigning variables
+user_name = input("What is your name? ")
+length = len(user_name)
+print(length)
+
