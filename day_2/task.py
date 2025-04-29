a17d57aa48927e766588e1272cf673642b7dcd0a
Bektas Erkan Florin <b.e.florin@outlook.com>
2025-04-29 17:32:29 +0300

    Add files via upload

---
+++ day_2/task.py
@@ -0,0 +1,26 @@
+# Subscripting
+print(("Hello"[4]))
+
+# Integer
+print(998 + 765)
+
+# Large Integer
+print(123_456_789)
+
+# Floats
+print(3.14159)
+
+# Exercise
+print(len(str(12345)))
+
+# Checking the data type
+print(type("Hello"))
+print(type(1234))
+print(type(3.145))
+print(type(True))
+
+# Conversing data types
+print(int("1234") + int("3456"))
+
+# Exercise
+print("number of letters in your name: " + str(len(input("What is your name? "))))
