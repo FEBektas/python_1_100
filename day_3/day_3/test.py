a024e35a65bbe2da510143efafc5e5bbf2fd996a
Bektas Erkan Florin <b.e.florin@outlook.com>
2025-07-07 13:03:27 +0300

    Add files via upload

--- day_3/day_3/test.py
+++ day_3/day_3/test.py
@@ -1,26 +1,48 @@
-print("Hello and Wellcome to the, TinyWonderLand.\n")
-print("In order to ride in our park, you'll need to be over 120 cm.\n")
+# Added the ASCII art to another python file in the directory and I have imported it to look cleaner.
+import time
+from art import dungeon
 
-height = int(input("What is your height? "))
+# Printed the ASCII art to look more alive, and added time.sleep to give it a felling, like someone is really talking with you.
+print(dungeon)
+time.sleep(1)
 
-if height >= 120:
-    print("You can ride in our Park, but before that we need to know your age. Because we have special prices!\n")
-    price = 0
-    age = int(input("What is your age? "))
-    photos = input("Before riding, do you want to get at the end some picture?")
-    if photos == "yes" and photos == "y":
-        if 45 <= age < 55:
-            price = 0
-            print("Wellcome, as of your age the tickets are on us!")
-        elif age >= 18:
-            price =+ 12
-            print(f"Young man you'll have to pay {price}$ before riding!")
-        elif age < 12:
-            price =+ 5
-            print(f"Young one, you'll need to pay {price}$ before riding")
-        else:
-            price =+ 7
-            print(f"You need to pay {price}$")
-        price =+ 3
+print("Wellcome to the Treasure DUNGEONNN!"
+      " Your mission is you find the treasure and escape ALIVE!\nGood Luck Explorer!!")
+time.sleep(2)
+# * using a backslash (\) in front of a useful symbol, then the program will think that we try to add that symbol and it will not read it as code.
+cross_road = input("You're at a cross road leading in 3 directions. "
+                       "'Left', 'right' and 'straight',Were are you choosing to go?\n").lower()
+
+# The logic of the game
+if cross_road == "left":
+    print("You've fallen into a snake pit, you have DIED!\nGame Over")
+elif cross_road == "right":
+    print("A Giant Goblin is wondering the room. He saw you and crushed you, you have DIED!\nGame Over")
+elif cross_road == "straight":
+    print("There we're no dangers, the roads seems good, until you reached a dead end but with a pond.")
 else:
-    print("Unfortunately you are a little short to rider here.\ncome back when you'll grow taller.")
+    print("You glitched out of the Dungeon and Died.... Game Over")
+# The user has reached the next level.
+pond = input("You can wait or you can swim, what will be your decision?\n").lower()
+
+if pond == "swim":
+    print("There we're sharks in the water, and they ate your flash, you have died. Game Over")
+elif pond == "wait":
+    print("The wall was an illusion and because you waited it has Disappeared, you can pass through!")
+# adding time.sleep to add a little feeling, to feel more alive
+time.sleep(2)
+
+# The user has reached a room with 3 doors.
+print("You have reached the final level before you can find the treasure, but you'll have to choose the"
+              "right door in order to grab that treasureee")
+doors = input("The Doors are, 'Blue', 'Yellow' or 'Red'.\nWhich is the correct door and which would you choose?\n").lower()
+
+if doors == "blue":
+    print("An ice goblin has appear, turning you into frozen dust. You have died, Game Over")
+elif doors == "yellow":
+    print("A Thunder God Dragon has appeared, making you dust before you can see him. You have died, Game Over")
+elif doors == "red":
+    print("You notice that no danger is sensing in the air. And you can look directly at the Treasure."
+          "")
+
+print("You took the Treasure and escaped alive. Congratulations Treasurer, you are now a very wealthy being.")
