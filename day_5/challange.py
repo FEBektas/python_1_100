570cf3d5a5a65fefd81b4fdc0bf52eda4fa32c6d
Bektas Erkan Florin <b.e.florin@outlook.com>
2025-10-02 13:36:46 +0300

    Add files via upload

--- day_5/challange.py
+++ day_5/challange.py
@@ -2,10 +2,19 @@
 
 student_scores = [150,124,180,165,173,189,169,146]
 
-max_scor = 0 # creating a variable to hold the intermediate values until we reach maximum value.
+max_score = 0 # creating a variable to hold the intermediate values until we reach maximum value.
+min_score = 0
 
 for x in student_scores:
-    if x > max_scor: # if statement to see if the item in our list is higher than our declared variable.
-        max_scor = x # If our number is higher than our variable (0), setting the variable to that value until it reaches the highest number
+    if x > max_score: # if statement to see if the item in our list is higher than our declared variable.
+        max_score = x # If our number is higher than our variable (0), setting the variable to that value until it reaches the highest number
 
-print(max_scor)
+print(max_score)
+print(min_score)
+
+# The Gauss Challenge
+calcul = 0 # - Creating a variable to hold the value neutral.
+
+for number in range(1,101): # This for loop will take the numbers from 1-100
+    calcul += number # This will add every number from the for loop 
+print(calcul)
--- day_5/test.py
+++ day_5/test.py
@@ -19,3 +19,12 @@ for x in student_scores:
     summ += x
 
 print(summ)
+
+# Range function.
+for number in range(1,11): # - created a for loop that will go through all the numbers from start to finish one by one.
+    print(number)
+
+print("-----")
+
+for numbers in range (1, 101 ,5): # For the loop to increase its counting from one to another number just set a comma "," and type what number we want.
+    print(numbers)
